ea9fa79965ca364b0c4cda65f22bf633beec65aa
JulieKihumba <151759770+JulieKihumba@users.noreply.github.com>
2024-08-19 06:32:39 +0300

    "Updated Hero component to display image and text content"

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import hero from '../assets/Images/hero.jpg';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className="">
+            <img src={hero} alt="" className='size-3/5 mx-auto my-10'/>
+        </div>
+        <div className="pl-20 space-y-3 max-w-sm">
+            <h1 className='text-black text-3xl font-semibold'>Online Experiences</h1>
+            <p className='text-xl font-light'>Join unique interactive activities led by one-of-a-kind hosts-all without living home</p>
+        </div>
+    </div>
   )
 }
 
